3eed38de47fbb629ab7c6092666c9b849a8918d9
Colin McCormack <mcccol@gmail.com>
2010-05-16 10:09:21 +0000

    * added coroutine shim

--- Wub/Httpd.tcl
+++ Wub/Httpd.tcl
@@ -336,7 +336,9 @@ namespace eval Httpd {
 	    dict set reply -charset $charset
 	    dict set reply -chconverted $charset
 	    dict set reply content-type "$ct; charset=$charset"
-	    dict set reply -content [encoding convertto $charset [dict get $reply -content]]
+	    if {[string tolower $charset] ne [encoding system]} {
+		dict set reply -content [encoding convertto $charset [dict get $reply -content]]
+	    }
 	}
 	return $reply
     }
