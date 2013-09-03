50f43f917975445617cb886875f93f7aa0df159c
Colin McCormack <mcccol@gmail.com>
2013-09-04 06:55:49 +1000

    * more template/context attempts.

--- Domains/Mason/Mason.tcl
+++ Domains/Mason/Mason.tcl
@@ -128,9 +128,11 @@ class create ::Mason {
 	variable context
 	set code [catch {
 	    #puts stderr "Mason template: $template"
-	    namespace eval $context "upvar response response; subst $template"
+	    namespace eval $context "set response $response"
+	    namespace eval $context "subst $template"
 	} result eo]	;# result is the substituted template
 	Debug.mason {template result: $code ($eo) - '$result' over '$template'} 2
+	set response ${context}::response
 
 	if {$code && $code < 200} {
 	    dict set response -dynamic 1
