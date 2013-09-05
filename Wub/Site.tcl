365d6e4d0edfc9826187848a5ce6a5b9145a9907
Colin McCormack <mcccol@gmail.com>
2013-09-06 09:43:26 +1000

    * Steve Havelka's patch to support .tm files with patch config var

--- Wub/Site.tcl
+++ Wub/Site.tcl
@@ -393,6 +393,7 @@ namespace eval ::Site {
 	# append $path to ::auto_path - allow site.config to augment $auto_path
 	if {[info exists path] && $path ne ""} {
 	    lappend ::auto_path {*}$path
+	    ::tcl::tm::path add {*}$path
 	}
 	if {[info exists require] && $require ne ""} {
 	    foreach r $require {
